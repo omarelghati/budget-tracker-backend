7fa5b10cbef8675b106362fa870e23096b1ad157
Omar EL GHATI <omar.elghati@dentsuaegis.com>
2021-01-02 13:40:48 +0100

    connection string 2

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,8 @@
 // require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = "mongodb://localhost/budget-tracker-db";
+const MONGODB_URI =
+  "mongodb+srv://omarUser:XPpj9_4_ZkqdWM5@cluster0.po8a9.mongodb.net/budget-tracker?retryWrites=true&w=majority";
 const JWT_SECRET = "Qzp752RSBNxSK8fYNA1c";
 const SALT_ROUNDS = 10;
 
