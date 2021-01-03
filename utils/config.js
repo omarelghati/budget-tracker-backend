5a5b155fc7ab21cfed8da96b7714313559ac1a1d
Omar EL GHATI <omar.elghati@dentsuaegis.com>
2021-01-03 02:56:51 +0100

    use the ip address of the container

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,23 @@
 // require("dotenv").config();
+var // Local ip address that we're trying to calculate
+  address,
+  // Provides a few basic operating-system related utility functions (built-in)
+  os = require("os"),
+  // Network interfaces
+  ifaces = os.networkInterfaces();
+
+// Iterate over interfaces ...
+for (var dev in ifaces) {
+  // ... and find the one that matches the criteria
+  var iface = ifaces[dev].filter(function (details) {
+    return details.family === "IPv4" && details.internal === false;
+  });
+
+  if (iface.length > 0) address = iface[0].address;
+}
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = `mongodb://192.168.1.200:27017/budget-tracker`;
+const MONGODB_URI = `mongodb://${address}:27017/budget-tracker`;
 const JWT_SECRET = "Qzp752RSBNxSK8fYNA1c";
 const SALT_ROUNDS = 10;
 
