1854ad5d4471484a56b50faf238bdb412b57c133
omarelghati <omarelghati@users.noreply.github.com>
2021-05-01 17:59:40 +0000

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = `mongodb+srv://omar_dbuser:Ir4cyGe4e8u81Gw9@budget-tracker-cluster.mnl3o.mongodb.net/budget-tracker?retryWrites=true&w=majority`;
+const MONGODB_URI = "mongodb+srv://omar_dbuser:Ir4cyGe4e8u81Gw9@budget-tracker-cluster.mnl3o.mongodb.net/budget-tracker?retryWrites=true&w=majority";
 const JWT_SECRET = "Qzp752RSBNxSK8fYNA1c";
 const SALT_ROUNDS = 10;
 
